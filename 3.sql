03f1d13a9c037fd5774f79a74d4f6575485a86d0
abdulsamadmj <73947768+abdulsamadmj@users.noreply.github.com>
2021-10-08 10:38:49 +0530

    Create 3.sql
    3. Create a table Hospital with the fields
    (doctorid,doctorname,department,qualification,experience).
    Write the queries to perform the following.
    a) Insert 5 records
    b) Display the details of Doctors
    c) Display the details of doctors who have the qualification ‘MD’
    d) Display all doctors who have more than 5 years experience but do not have the
    qualification ‘MD’
    e) Display the doctors in ‘Skin’ department
    f) update the experience of doctor with doctored=’D003’ to 5
    g) Delete the doctor with DoctorID=’D005’

---
+++ 3.sql
@@ -0,0 +1,46 @@
+#3)
+DROP DATABASE IF EXISTS hospital;
+
+CREATE DATABASE hospital;
+
+USE hospital;
+
+CREATE TABLE hospital(
+	doctorid VARCHAR(5) PRIMARY KEY,
+	doctorname VARCHAR(20) NOT NULL,
+	department VARCHAR(15),
+	qualification VARCHAR(10),
+	experience INT(2)
+);
+
+DESC hospital;
+
+#a)
+INSERT INTO hospital(doctorid,doctorname,department,qualification,experience)
+	 VALUES("D001","Eden Sadler","Skin","MD",6),
+    	("D002","Kristina Randolph","Heart","SD",7),
+    	("D003","Arla Lugo","Stomach","MD",4),
+    	("D004","Sheridan Aguilar","Skin","SD",8),
+    	("D005","Elvis Person","Skin","SD",8);
+
+#b)
+SELECT * FROM hospital;
+
+#c)
+SELECT * FROM hospital WHERE qualification="MD";
+
+#d)
+SELECT * FROM hospital WHERE qualification!="MD" AND experience>5;
+
+#e)
+SELECT * FROM hospital WHERE department="Skin";
+
+#f)
+UPDATE hospital SET experience=5 WHERE doctorid="D003";
+
+SELECT * FROM hospital WHERE doctorid="D003";
+
+#g)
+DELETE FROM hospital WHERE doctorid="D005";
+
+SELECT * FROM hospital;
